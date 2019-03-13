c81382dc768cb8ea40a4f2654472cbaf47fdb1f9
Oskarthansen <oskarthansen@outlook.dk>
2019-03-13 11:42:53 +0100

    Made Receiver

---
+++ ATC/ATC/Transponderreceiver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TransponderReceiver;
+
+namespace ATC
+{
+    public class TransponderReceiver
+    {
+        
+        public ITransponderReceiver Receiver = TransponderReceiverFactory.CreateTransponderDataReceiver();
+        
+    }
+
+    
+}
