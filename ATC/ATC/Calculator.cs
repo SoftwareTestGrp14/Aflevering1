fe8cb348fbe1966783f2bf0556d8a828326611b9
Jonas <au544262@post.au.dk>
2019-03-13 12:29:37 +0100

    Calculator added

---
+++ ATC/ATC/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATC
+{
+    class Calculator
+    {
+        public void CalcVelocityAndCourse(int xCord, int yCord, int alt)
+        {
+
+        }
+
+    }
+}
