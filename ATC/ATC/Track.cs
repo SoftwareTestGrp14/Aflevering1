e503939f66803dd8dc10e9b5bd2b9dc665cd995f
Andreas <201608930@post.au.dk>
2019-03-13 11:03:12 +0100

    ITrack, Track og SeperationChecker tilføjet

---
+++ ATC/ATC/Track.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATC
+{
+    class Track
+    {
+        public Track(string tag, int xC, int yC, int alt, int vel, int course, DateTime ts)
+        {
+            _tag = tag;
+            _xCord = xC;
+            _yCord = yC;
+            _alt = alt;
+            _velocity = vel;
+            _course = course;
+            _timestamp = ts;
+        }
+
+        private string _tag { get; }
+        private int _xCord { get; }
+        private int _yCord { get; }
+        private int _alt { get; }
+        private int _velocity { get; }
+        private int _course { get; }
+        private DateTime _timestamp { get; }
+    }
+}
