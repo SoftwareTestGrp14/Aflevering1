97d5599604101ca558a2ae090ba81b84fbc9a171
Jonas <au544262@post.au.dk>
2019-03-13 11:41:35 +0100

    Added planetracker

---
+++ ATC/ATC/PlaneTracker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ATC
+{
+    public class PlaneTracker : IPlaneTracker
+    {
+        
+        private List<ITrack> tracks;
+        private ISeparationChecker sepCheck;
+        
+        public void Update(string data)
+        {
+            string[] separatedData = ConvertTransponderData(data);
+            
+            ITrack newTrack = new Track(separatedData[0], separatedData[1], separatedData[2], separatedData[3], separatedData[4], separatedData[5]);
+
+            tracks.Add(newTrack);
+
+
+        }
+
+        public string[] ConvertTransponderData(string data)
+        {
+            
+
+            string[] separatedData = data.Split(new string[] { "; " }, StringSplitOptions.None);
+
+
+            return separatedData;
+        }
+
+        
+        public bool CheckSeparation(List<ITrack> tracks)
+        {
+            sepCheck.isSeparationCondition(tracks);
+        }
+        
+    }
+}
