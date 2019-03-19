58436a8cbc52f4c6dd125b201ed3fa2b08ce22e7
Jonas <au544262@post.au.dk>
2019-03-19 13:00:09 +0100

    Test a separationcondition

---
+++ ATC/UnitTests/TestSeparationCondition.cs
@@ -0,0 +1,27 @@
+﻿using ATC;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class TestSeparationCondition
+    {
+        private SeparationCondition _uut;
+        private ITrack _fakeTrack1;
+        private ITrack _fakeTrack2;
+
+        [SetUp]
+        public void Setup()
+        {
+          
+    
+            _uut = new SeparationCondition();
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+            Assert.Pass();
+        }
+    }
+}
